fe934fd128bc94f89f4fddd9234d721592468309
pedrotp <pedro@pedrotp.com>
2017-06-11 01:57:39 -0400

    removed projects to test

--- server.js
+++ server.js
@@ -42,25 +42,29 @@ app.get('/lab/:id', function(req,res){
   var context = {};
   pool.query('SELECT name FROM `lab` WHERE id=?', [req.params.id]).then(function (results, fields) {
     context.name = results[0];
+    context.projects = [];
   }).then(function () {
     return pool.query('SELECT * FROM `employee` WHERE lab_id=? ORDER BY id',[req.params.id]);
   }).then(function (results, fields) {
     context.employees = results;
-  }).then(function() {
-    return pool.query('SELECT * FROM `project` WHERE lab_id=? ORDER BY id',[req.params.id]);
-  }).then(function (results, fields) {
-    context.projects = results;
-  }).then(function() {
+  })
+  // .then(function() {
+  //   return pool.query('SELECT  FROM `project` INNER JOIN `employee_project` ON project.id = employee_project.project_id INNER JOIN `employee` ON employee_project.employee_id = employee.id WHERE employee.lab_id=? ORDER BY id',[req.params.id]);
+  // }).then(function (results, fields) {
+  //   context.projects = results;
+  // })
+  .then(function() {
     return pool.query('SELECT * FROM `equipment` WHERE lab_id=? ORDER BY id',[req.params.id]);
   }).then(function (results, fields) {
     context.equipment = results;
+  }).then(function () {
+    res.render('lab', context);
   }).catch(function (err) {
       console.error(err.stack);
       res.type('plain/text');
       res.status(500);
       res.render('500', { title: '500: SERVER ERROR' });
   });
-  res.render('lab', context);
 });
 
 function selectAll(res, callback){
@@ -85,29 +89,6 @@ app.post('/lab', function (req, res, next) {
     selectAll(res, function() {
       res.sendStatus(200);
     });
-  });
-});
-
-app.put('/workouts', function(req, res, next) {
-  console.log(req.body);
-  pool.query("SELECT * FROM workouts WHERE id=?", [req.body.id], function(err, result){
-    if(err){
-      next(err);
-      return;
-    }
-    if(result.length){
-      var current = result[0];
-      console.log(current);
-      pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=?",
-        [req.body.name || current.name, req.body.reps || current.reps, req.body.weight || current.weight, req.body.date || current.date, req.body.lbs || current.lbs, req.body.id],
-        function (err, result){
-        if(err){
-          next(err);
-          return;
-        }
-        res.status(200).send("Updated " + result.changedRows + " rows.");
-      });
-    }
   });
 });
 
@@ -143,106 +124,6 @@ app.post('/reset-tables',function (req,res,next){
     }
   });
 
-  // var queryStr = "CREATE TABLE `lab` ("+
-  //   "`id` int(11) NOT NULL AUTO_INCREMENT,"+
-  //   "`name` varchar(255) DEFAULT NULL,"+
-  //   "`ext` int(11) DEFAULT NULL,"+
-  //   "PRIMARY KEY (`id`)"+
-  // ") ENGINE=InnoDB;";
-  //
-  // pool.query(queryStr).then(function(){
-  //   queryStr = "CREATE TABLE `employee` ("+
-  //     "`id` int(11) NOT NULL AUTO_INCREMENT,"+
-  //     "`first_name` varchar(255) NOT NULL,"+
-  //     "`last_name` varchar(255) DEFAULT NULL,"+
-  //     "`ext` int(11) DEFAULT NULL,"+
-  //     "`lab_id` int(11) DEFAULT NULL,"+
-  //     "`manager_id` int(11) DEFAULT NULL,"+
-  //     "PRIMARY KEY (`id`),"+
-  //     "CONSTRAINT `fk_employee_lab`"+
-  //     "  FOREIGN KEY (`lab_id`)"+
-  //     "  REFERENCES `lab` (`id`)"+
-  //     "  ON DELETE SET NULL"+
-  //     "  ON UPDATE CASCADE,"+
-  //     "CONSTRAINT `fk_employee_manager`"+
-  //     "  FOREIGN KEY (`manager_id`)"+
-  //     "  REFERENCES `employee` (`id`)"+
-  //     "  ON DELETE SET NULL"+
-  //     "  ON UPDATE CASCADE"+
-  //   ") ENGINE=InnoDB;";
-  //   return pool.query(queryStr);
-  // }).then(function () {
-  //   queryStr = "CREATE TABLE `equipment` ("+
-  //     "`id` int(11) NOT NULL AUTO_INCREMENT,"+
-  //     "`maintainer_id` int(11) DEFAULT NULL,"+
-  //     "`calibration_date` date,"+
-  //     "`purchase date` date,"+
-  //     "`lab_id` int(11) DEFAULT NULL,"+
-  //     "PRIMARY KEY (`id`),"+
-  //     "CONSTRAINT `fk_equipment_lab`"+
-  //     "  FOREIGN KEY (`lab_id`)"+
-  //     "  REFERENCES `lab` (`id`)"+
-  //     "  ON DELETE SET NULL"+
-  //     "  ON UPDATE CASCADE,"+
-  //     "CONSTRAINT `fk_equipment_employee`"+
-  //     "  FOREIGN KEY (`maintainer_id`)"+
-  //     "  REFERENCES `employee` (`id`)"+
-  //     "  ON DELETE SET NULL"+
-  //     "  ON UPDATE CASCADE"+
-  //   ") ENGINE=InnoDB;";
-  //   return pool.query(queryStr);
-  // }).then(function () {
-  //   queryStr = "CREATE TABLE `project` ("+
-  //     "`id` int(11) NOT NULL AUTO_INCREMENT,"+
-  //     "`request_date` date,"+
-  //     "`due_date` date,"+
-  //     "`objective` text,"+
-  //     "PRIMARY KEY (`id`)"+
-  //   ") ENGINE=InnoDB;";
-  //   return pool.query(queryStr);
-  // }).then(function () {
-  //   queryStr = "CREATE TABLE `project_equipment` ("+
-  //     "`project_id` int(11) NOT NULL,"+
-  //     "`equipment_id` int(11) NOT NULL,"+
-  //     "PRIMARY KEY (`project_id`,`equipment_id`),"+
-  //     "CONSTRAINT `fk_projeq_project`"+
-  //     "  FOREIGN KEY (`project_id`)"+
-  //     "  REFERENCES `project` (`id`)"+
-  //     "  ON DELETE CASCADE"+
-  //     "  ON UPDATE CASCADE,"+
-  //     "CONSTRAINT `fk_projeq_equipment`"+
-  //     "  FOREIGN KEY (`equipment_id`)"+
-  //     "  REFERENCES `equipment` (`id`)"+
-  //     "  ON DELETE CASCADE"+
-  //     "  ON UPDATE CASCADE"+
-  //   ") ENGINE=InnoDB;";
-  //   return pool.query(queryStr);
-  // }).then(function () {
-  //   queryStr = "CREATE TABLE `employee_project` ("+
-  //     "`employee_id` int(11) NOT NULL,"+
-  //     "`project_id` int(11) NOT NULL,"+
-  //     "PRIMARY KEY (`employee_id`,`project_id`),"+
-  //     "CONSTRAINT `fk_emplproj_employee`"+
-  //     "  FOREIGN KEY (`employee_id`)"+
-  //     "  REFERENCES `employee` (`id`)"+
-  //     "  ON DELETE CASCADE"+
-  //     "  ON UPDATE CASCADE,"+
-  //     "CONSTRAINT `fk_emplproj_project`"+
-  //     "  FOREIGN KEY (`project_id`)"+
-  //     "  REFERENCES `project` (`id`)"+
-  //     "  ON DELETE CASCADE"+
-  //     "  ON UPDATE CASCADE"+
-  //   ") ENGINE=InnoDB;";
-  //   return pool.query(queryStr);
-  // }).then(function() {
-  //   res.status(200).send();
-  // }).catch(function (err) {
-  //   console.error(err.stack);
-  //   res.type('plain/text');
-  //   res.status(500);
-  //   res.render('500', { title: '500: SERVER ERROR' });
-  // });
-
 });
 
 app.use(morgan('dev'));
@@ -262,3 +143,27 @@ app.use(function(err, req, res, next){
 app.listen(app.get('port'), function(){
   console.log('Express started on http://flip3.engr.oregonstate.edu:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
+
+
+// app.put('/workouts', function(req, res, next) {
+//   console.log(req.body);
+//   pool.query("SELECT * FROM workouts WHERE id=?", [req.body.id], function(err, result){
+//     if(err){
+//       next(err);
+//       return;
+//     }
+//     if(result.length){
+//       var current = result[0];
+//       console.log(current);
+//       pool.query("UPDATE workouts SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=?",
+//         [req.body.name || current.name, req.body.reps || current.reps, req.body.weight || current.weight, req.body.date || current.date, req.body.lbs || current.lbs, req.body.id],
+//         function (err, result){
+//         if(err){
+//           next(err);
+//           return;
+//         }
+//         res.status(200).send("Updated " + result.changedRows + " rows.");
+//       });
+//     }
+//   });
+// });
