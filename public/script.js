db6cfbbf06359e76391f3f49efacdab2de0e81e0
pedrotp <pedro@pedrotp.com>
2017-06-11 22:50:13 -0400

    added loading generic modal

--- public/script.js
+++ public/script.js
@@ -205,8 +205,9 @@ $('.container').on('click', 'a.employee-id', function (event) {
   $.ajax({
     method: 'GET',
     url: path + 'employee/' + $(this).attr('data-employee-id')
-  })
-  .done(function (result) {
+  }).done(function (result) {
+    console.log('result', result);
+    result = JSON.parse(result);
     $('#loadingModal').find('h4').text(result.first_name + " " + result.last_name);
     $('#loadingModal').find('div.modal-body').html('<table class="table table-bordered table-condensed table-striped table-responsive"><tr><th class="text-center">First Name</th><th class="text-center">Last Name</th><th class="text-center">Extension</th></tr><tr><td><p class="text-center">' + result.first_name + '</p></td><td><p class="text-center">' + result.last_name + '</p></td><td><p class="text-center">' + result.ext + '</p></td></tr></table>');
   });
